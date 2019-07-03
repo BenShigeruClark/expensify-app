9a8bb0ac1e167e379f26844bd9d9dc484e6f5b6a
Ben <benclarkphoto@mac.com>
2019-07-03 16:19:43 -0700

    ES6 Array destructuring

--- src/playground/destructuring.js
+++ src/playground/destructuring.js
@@ -1,3 +1,5 @@
+// Object Destructuring
+
 // const person = {
 //   name: 'Benjamin',
 //   age: 46,
@@ -15,13 +17,28 @@
 //   console.log(`Its ${temperature} degrees in ${city}.`);
 // }
 
-const book = {
-  title: 'Ego is the Enemy',
-  author: 'Ryan Holiday',
-  publisher: {
-    name: 'Penguin'
-  }
-};
+// const book = {
+//   title: 'Ego is the Enemy',
+//   author: 'Ryan Holiday',
+//   publisher: {
+//     name: 'Penguin'
+//   }
+// };
 
-const {name: publisherName = 'Self-Published'} = book.publisher;
-console.log(publisherName); // Penguin, Self-Published
+// const {name: publisherName = 'Self-Published'} = book.publisher;
+// console.log(publisherName); // Penguin, Self-Published
+
+
+//
+// Array destructuring
+//
+
+// const address = ['1299 S Juniper Street', 'Huntington Beach', 'California', '92648'];
+// const [, city, state = 'New York'] = address;
+// console.log(`You are in ${city}, ${state}.`);
+
+const item = ['Coffee (iced)', '$3.00', '$3.50', '$3.75'];
+const [itemName, , mediumPrice] = item;
+
+// grab first and third items using array destructuring
+console.log(`A medium ${itemName} costs ${mediumPrice}.`);
