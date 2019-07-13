6aea8818803d89fbbf53271a671867362afac12f
Ben <benclarkphoto@mac.com>
2019-07-12 21:54:23 -0700

    Setting up jest

--- src/tests/add.test.js
+++ src/tests/add.test.js
@@ -1,5 +1,17 @@
 const add = (a, b) => a + b;
+const generateGreeting = (name = 'Anonymous') => `Hello ${name}!`;
 
 test('should add two numbers', () => {
   const result = add(3, 4);
+  expect(result).toBe(7);
+});
+
+test('Should generate a string!', () => {
+  const result = generateGreeting('Ben');
+  expect(result).toBe('Hello Ben!');
+});
+
+test('should generate greeting for no name', () => {
+  const result = generateGreeting();
+  expect(result).toBe('Hello Anonymous!');
 });
