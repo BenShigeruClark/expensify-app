202cbb98f6b89f07fe0d577f06d79bb84ed44d15
Ben <benclarkphoto@mac.com>
2019-07-13 23:52:54 -0700

    remove comments

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -1,6 +1,6 @@
 import uuid from 'uuid';
 
-// ADD_EXPENSE
+
 export const addExpense = (
   { 
     description = '', 
@@ -19,13 +19,11 @@ export const addExpense = (
   }
 });
 
-// REMOVE_EXPENSE
 export const removeExpense = ({ id } = {}) => ({
   type: 'REMOVE_EXPENSE',
   id
 });
 
-// EDIT_EXPENSE
 export const editExpense = (id, updates) => ({
   type: 'EDIT_EXPENSE',
   id,
