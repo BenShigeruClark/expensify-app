699e81cc16e93814b7d29f49a411ea102c8e1b11
Ben <benclarkphoto@mac.com>
2019-07-18 23:46:46 -0700

    changes to playground folder

--- src/playground/destructuring.js
+++ src/playground/destructuring.js
@@ -37,8 +37,8 @@
 // const [, city, state = 'New York'] = address;
 // console.log(`You are in ${city}, ${state}.`);
 
-const item = ['Coffee (iced)', '$3.00', '$3.50', '$3.75'];
-const [itemName, , mediumPrice] = item;
+// const item = ['Coffee (iced)', '$3.00', '$3.50', '$3.75'];
+// const [itemName, , mediumPrice] = item;
 
 // grab first and third items using array destructuring
-console.log(`A medium ${itemName} costs ${mediumPrice}.`);
+// console.log(`A medium ${itemName} costs ${mediumPrice}.`);
