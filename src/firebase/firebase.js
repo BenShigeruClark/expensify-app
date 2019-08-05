8cdd67fb125b0664ba7c0264b9ba04268736588e
Ben <benclarkphoto@mac.com>
2019-08-04 23:15:37 -0700

    Promises with Firebase

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -22,16 +22,24 @@ database.ref().set({
     city: 'Huntington Beach',
     country: 'United States'
   }
+}).then(() => {
+  console.log('Data is saved!');
+}).catch((e) => {
+  console.log('This failed.', e);
 });
 
 // database.ref().set('This is my data.');
 
-database.ref('age').set(27);
-database.ref('location/city').set('Long Beach');
+// database.ref('age').set(27);
+// database.ref('location/city').set('Long Beach');
 
 database.ref('attributes').set({
   height: 74,
   weight: 120
+}).then(() => {
+  console.log('Second set call worked!');
+}).catch((e) => {
+  console.log('Failure, please try again', e);
 });
 
-console.log('I made a request to change the data.');
+
