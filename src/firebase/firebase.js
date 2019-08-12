1d1468ec5186fdf65712ab4cb09e1a5b171327e2
Ben <benclarkphoto@mac.com>
2019-08-11 22:30:38 -0700

    Create seperate test database

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,13 +1,13 @@
 import * as firebase from 'firebase';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC79oDDX-9XcN8UBTFYsO8jS9aICQMWyGE",
-  authDomain: "expensify-d476a.firebaseapp.com",
-  databaseURL: "https://expensify-d476a.firebaseio.com",
-  projectId: "expensify-d476a",
-  storageBucket: "",
-  messagingSenderId: "149557498475",
-  appId: "1:149557498475:web:e9f7c7e64d09dc16"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID
 };
 
 firebase.initializeApp(firebaseConfig);
