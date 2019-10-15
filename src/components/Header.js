2d885f43d3794c424c871451efc3fb7c76b20a16
Ben <benclarkphoto@mac.com>
2019-10-14 23:45:55 -0700

    add header button

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,7 +10,7 @@ export const Header = ({ startLogout }) => (
         <Link className="header__title" to="/dashboard">
           <h1>Expensify</h1>
         </Link>
-        <button onClick={startLogout}>Logout</button>
+        <button className="button-partial button-link" onClick={startLogout}>Logout</button>
       </div>
     </div>
       {/* <NavLink to="/help" activeClassName="is-active">Help</NavLink> */}
